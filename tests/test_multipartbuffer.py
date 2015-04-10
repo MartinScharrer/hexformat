989d0747267ec3c4c3438701083613503e166009
Martin Scharrer <martin.scharrer@web.de>
2015-04-10 12:43:12 +0200

    Added copyright notices to all source files.

--- tests/test_multipartbuffer.py
+++ tests/test_multipartbuffer.py
@@ -1,3 +1,24 @@
+""" Unit tests for multipartbuffer module.
+
+  License::
+  
+    Copyright (C) 2015  Martin Scharrer <martin@scharrer-online.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 from hexformat.multipartbuffer import MultiPartBuffer, Buffer
 from hexformat import IntelHex, RandomContent
 import sys
